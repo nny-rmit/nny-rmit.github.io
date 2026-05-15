ada92f45138a9f8229d09ed96fd48f04376c0986
nny-rmit <s4172736@student.rmit.edu.au>
2026-05-15 18:09:53 +1000

    Updated Assn

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,19 +1,98 @@
-const musicSelector = document.querySelector("#musicsel");
-console.log(myVideo);
+const myMusic = document.querySelector("#myMusic");
 
-//-----------------------------------------------------------------------------------------
-//progress bar
-const progressBar = document.querySelector("#progress-bar");
-console.log(progressBar);
+//--------------------------------------------------------------------------------------------------------------
+//playlist functionality
 
-myVideo.addEventListener("timeupdate", updateProgress);
+const playlist = [
+  {
+    id: 1,
+    src: "https://file.garden/aGJwdAK8lRt7Ht-6/Assignment2/y%20u%20so%20fake.wav",
+    name: "y u so fake",
+  },
+  {
+    id: 2,
+    src: "https://file.garden/aGJwdAK8lRt7Ht-6/Assignment2/tense.wav",
+    name: "tense",
+  },
+  {
+    id: 3,
+    src: "https://file.garden/aGJwdAK8lRt7Ht-6/Assignment2/selamat%20pagi.wav",
+    name: "selamat pagi",
+  },
+  {
+    id: 4,
+    src: "https://file.garden/aGJwdAK8lRt7Ht-6/Assignment2/DurryHouse.wav",
+    name: "durryHouse",
+  },
+  {
+    id: 5,
+    src: "https://file.garden/aGJwdAK8lRt7Ht-6/Assignment2/Fentship.wav",
+    name: "fentship",
+  },
+  {
+    id: 6,
+    src: "https://file.garden/aGJwdAK8lRt7Ht-6/Assignment2/Rather%20KMS.wav",
+    name: "rather KMS",
+  },
+];
 
-function updateProgress() {
-  const duration = (myVideo.currentTime / myVideo.duration) * 100;
-  progressBar.style.width = duration + "%";
+const msg = document.querySelector("#msg");
+function chooseMusic(id) {
+  //retrieve the song that is currently playing
+  console.log(playlist[id].src);
+  //changes the name of the track that is playing
+  msg.textContent = playlist[id].name;
+  myMusic.src = playlist[id].src;
+  myMusic.load();
+  myMusic.play();
 }
-//-----------------------------------------------------------------------------------------
-//play and pause
+
+//changing between the songs using the text
+const music1Button = document.querySelector("#music1-button");
+console.log(music1Button);
+
+music1Button.addEventListener("click", function () {
+  chooseMusic(0);
+});
+
+const music2Button = document.querySelector("#music2-button");
+console.log(music2Button);
+
+music2Button.addEventListener("click", function () {
+  chooseMusic(1);
+});
+
+const music3Button = document.querySelector("#music3-button");
+console.log(music3Button);
+
+music3Button.addEventListener("click", function () {
+  chooseMusic(2);
+});
+
+const music4Button = document.querySelector("#music4-button");
+console.log(music4Button);
+
+music4Button.addEventListener("click", function () {
+  chooseMusic(3);
+});
+
+const music5Button = document.querySelector("#music5-button");
+console.log(music5Button);
+
+music5Button.addEventListener("click", function () {
+  chooseMusic(4);
+});
+
+const music6Button = document.querySelector("#music6-button");
+console.log(music6Button);
+
+music6Button.addEventListener("click", function () {
+  chooseMusic(5);
+});
+
+//--------------------------------------------------------------------------------------------------------------
+//play and pause functionality
+
 const playPauseButton = document.querySelector("#play-pause-button");
 console.log(playPauseButton);
 
@@ -23,16 +102,27 @@ const playPauseImg = document.querySelector("#play-pause-img");
 console.log(playPauseImg);
 
 function togglePlayback() {
-  if (myVideo.paused || myVideo.ended) {
-    myVideo.play();
+  if (myMusic.paused || myMusic.ended) {
+    myMusic.play();
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
   } else {
-    myVideo.pause();
+    myMusic.pause();
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v2.png";
   }
 }
-//-----------------------------------------------------------------------------------------
-// muting button
+//--------------------------------------------------------------------------------------------------------------
+//progress bar functionality
+const progressBar = document.querySelector("#progress-bar");
+console.log(progressBar);
+
+myMusic.addEventListener("timeupdate", updateProgress);
+
+function updateProgress() {
+  const duration = (myMusic.currentTime / myMusic.duration) * 100;
+  progressBar.style.width = duration + "%";
+}
+//--------------------------------------------------------------------------------------------------------------
+// muting functionality
 const muteUnmuteButton = document.querySelector("#mute-unmute-button");
 console.log(muteUnmuteButton);
 
@@ -42,57 +132,35 @@ const muteUnmuteImg = document.querySelector("#mute-unmute-img");
 console.log(muteUnmuteImg);
 
 function toggleAudio() {
-  if (myVideo.muted) {
-    myVideo.muted = false;
+  if (myMusic.muted) {
+    myMusic.muted = false;
     muteUnmuteImg.src =
       "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
   } else {
     //if video is unmuted, mute the audio + change the image
-    myVideo.muted = true;
+    myMusic.muted = true;
     muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
   }
 }
-
 //-----------------------------------------------------------------------------------------
-//fullscreen logic
-const fullscreenButton = document.querySelector("#fullscreen-button");
-console.log(fullscreenButton);
+//fast forward functionality
+//While not essential for a site like this , I felt this would fit the post-ironic, experimental vibe
+//the entire site exudes. The fast-forward function purely changes the playbackRate variable.
+const fastForwardButton = document.querySelector("#fast-forward-button");
+console.log(fastForwardButton);
 
-fullscreenButton.addEventListener("click", goFullscreen);
-myVideo.addEventListener("dblclick", goFullscreen);
+fastForwardButton.addEventListener("click", fastForward);
 
-function goFullscreen() {
-  if (!document.fullscreenElement) {
-    //checks if the video is fullscreen
-    //don't forget to add () after each function
-    myVideo.requestFullscreen();
+function fastForward() {
+  if (myMusic.playbackRate === 1.0) {
+    myMusic.playbackRate = 2.0;
   } else {
-    //if not, request if the video can go fullscreen
-    document.exitFullscreen();
+    myMusic.playbackRate = 1.0;
   }
 }
+
 //-----------------------------------------------------------------------------------------
-// timestamps , STEP LOGIC
-// marker 1
-const step1Button = document.querySelector("#step1-button");
-console.log(step1Button);
-
-step1Button.addEventListener("click", gotoStep1);
-
-//use the currentTime attribute
-function gotoStep1() {
-  myVideo.currentTime = 17.0;
-}
-//marker 2
-const step2Button = document.querySelector("#step2-button");
-
-step2Button.addEventListener("click", gotoStep2);
-
-function gotoStep2() {
-  myVideo.currentTime = 52.0;
-}
-//-----------------------------------------------------------------------------------------
-//like button
+//like functionality
 const heartButton = document.querySelector("#heart-button");
 console.log(heartButton);
 
@@ -100,70 +168,79 @@ let likesCount = 0;
 const likes = document.querySelector("#likes");
 console.log("likes");
 
-heartButton.addEventListener("click", showLikes);
+heartButton.addEventListener("click", showLikes, turnPink);
 function showLikes() {
   likesCount++;
-  //console.log(likesCount)
   likes.textContent = likesCount;
 }
-//-----------------------------------------------------------------------------------------
-//fast forward
-const fastForwardButton = document.querySelector("#fast-forward-button");
-console.log(fastForwardButton);
 
-fastForwardButton.addEventListener("click", fastForward);
-
-function fastForward() {
-  if (myVideo.playbackRate === 1.0) {
-    myVideo.playbackRate = 2.0;
-  } else {
-    myVideo.playbackRate = 1.0;
-  }
+function turnPink() {
+  heartButton.style.color = "#ff237b";
 }
 
-//-----------------------------------------------------------------------------------------------
-//playlist logic
+//-----------------------------------------------------------------------------------------
+//skip to start functionality
 
-// "name" is a parameter that can be retrieved later using textContent
-const videoList = [
-  { id: 1, src: "stardust.mp4", name: "stardust" },
-  { id: 2, src: "zenscape.mp4", name: "zenscape" },
-  {
-    id: 3,
-    src: "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/miac.mp4",
-    name: "music video",
-  },
-];
+const skipStartButton = document.querySelector("#skip-start-button");
+console.log(skipStartButton);
 
-//changing between the videos
-const stardustButton = document.querySelector("#stardust-vid-button");
-console.log(stardustButton);
+skipStartButton.addEventListener("click", skipStart);
 
-stardustButton.addEventListener("click", function () {
-  chooseVideo(0);
-});
+function skipStart() {
+  myMusic.currentTime = 0;
+}
 
-const zenscapeButton = document.querySelector("#zenscape-vid-button");
-console.log(zenscapeButton);
+//-----------------------------------------------------------------------------------------
+//volume functionality
+//i at first attempted to make this a Toggle button, but for some reason the if/else statement didn't
+//allow the volume to change back after it had been lowered (perhaps something wrong with the else statement)
+//so I ended up splitting it into 2 buttons. I have commented it out for archival purposes.
 
-zenscapeButton.addEventListener("click", function () {
-  chooseVideo(1);
-});
+const volumeDownButton = document.querySelector("#volume-down-button");
+console.log(volumeDownButton);
 
-const musicvideoButton = document.querySelector("#musicvideo-vid-button");
-console.log(musicvideoButton);
+volumeDownButton.addEventListener("click", volumeDown);
 
-musicvideoButton.addEventListener("click", function () {
-  chooseVideo(2);
-});
+function volumeDown() {
+  myMusic.volume = 0.1;
+}
 
-const msg = document.querySelector("#msg");
-function chooseVideo(id) {
-  //get the video that is currently playing
-  console.log(videoList[id].src);
-  //changes the name of the track that is playing
-  msg.textContent = "Now playing " + videoList[id].name;
-  myVideo.src = videoList[id].src;
-  myVideo.load();
-  //myVideo.play();
+const volumeUpButton = document.querySelector("#volume-up-button");
+console.log(volumeUpButton);
+
+volumeUpButton.addEventListener("click", volumeUp);
+
+function volumeUp() {
+  myMusic.volume = 1;
+}
+//const volumeImg = document.querySelector("#volume-img");
+//console.log(volumeImg);
+
+//function toggleVolume() {
+//if (myMusic.volume = 1) {
+//myMusic.volume = 0.2;
+//volumeImg.src = "https://img.icons8.com/ios-glyphs/30/high-volume--v1.png";
+//} else {
+//myMusic.volume = 1;
+//volumeImg.src = "https://img.icons8.com/ios-glyphs/30/low-volume.png";
+//}
+//}
+
+//-----------------------------------------------------------------------------------------
+//shuffle functionality
+
+//I implemented a simple random song shuffle function after searching up on Google
+//how to select random variables from an array, which I then just plugged into the
+//chooseMusic function.
+const shuffleButton = document.querySelector("#shuffle-button");
+console.log(shuffleButton);
+
+shuffleButton.addEventListener("click", shuffle);
+
+//This specific math.floor function is adapted from a line of code accessed here:
+//https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+function shuffle() {
+  const random = [Math.floor(Math.random() * playlist.length)];
+  console.log(random);
+  chooseMusic(random);
 }
